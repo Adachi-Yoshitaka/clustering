090331603ba5a21dfdd43ffb3349d132d86ca761
Adachi-Yoshitaka <111262347+Adachi-Yoshitaka@users.noreply.github.com>
2022-12-12 11:37:14 +0900

    Update hcluster.py

--- hcluster.py
+++ hcluster.py
@@ -136,7 +136,7 @@ def hcluster(features,distfcn=L2dist):
 
     # 新しいノードを生成する
     new_node = ClusterNode(new_vec,left=ni,right=nj,
-                           distance=closest,count=cnt)
+                           distance=closest,count=count)
     node.remove(ni)
     node.remove(nj)
     node.append(new_node)
